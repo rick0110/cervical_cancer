d93100e4505b0da26df8df3ccfe762c9d4558f47
richard viana <al.richard.viana@impatech.org.br>
2025-11-04 15:12:01 -0300

    __

--- src/models/script_train.py
+++ src/models/script_train.py
@@ -1,7 +1,10 @@
-# from train import *
+from train import *
 import os
 import pandas as pd
 import numpy as np
+import torch
+from sklearn.model_selection import train_test_split, KFold
+
 maps = {
     'carcinoma': 1,
     'dysplastic': 1,
@@ -26,8 +29,27 @@ def label_map_from_path(path):
 
 df['label'] = df['path'].apply(label_map_from_path)
 
-model
+df_train, df_val = train_test_split(df, test_size=0.1, stratify=df['label'], random_state=342)
 
+k_fold = KFold(n_splits=5, shuffle=True, random_state=42)
 
+models = {}  #-> model[ state_dict, history{loss_train, acc_train, loss_val, acc_val}, scores[val_acc, val_recall, val_precision], predictions[val_predictions, val_targets] ]
+fold = 1
 
+for train_index, val_index in k_fold.split(df):
+    df_train = df.iloc[train_index]
+    df_val = df.iloc[val_index]
 
+    model, history, scores, predictions = train_swinad2net(
+        train_df=df_train,
+        val_df=df_val,
+        num_classes=2,
+        image_size=(224, 224),
+        batch_size=32,
+        num_epochs=50,
+        learning_rate=1e-3,
+        checkpoint_dir='./checkpoints/kfold_experiment'
+    )
+    
+    models[f'fold_{fold}'] = [model.state_dict(), ]
+    fold += 1
